cda7e287f82e200acd3824b74dcbc43588cee38b
Jpeople <khieu.duongvan@gmail.com>
2025-05-30 17:38:24 +0900

    update g tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,19 @@ export default function RootLayout({
             `,
           }}
         />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LZTZ9CGQ1G"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LZTZ9CGQ1G');
+            `,
+          }}
+        />
+
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
