bcc592e5c7bfe850244cd888a1ecbad060d610c6
Jpeople <khieu.duongvan@gmail.com>
2025-12-16 10:39:54 +0900

    remove unuse component

--- src/components/features/video-generator/index.ts
+++
@@ -1,5 +0,0 @@
-export { VideoGenerator } from './VideoGenerator';
-export { ImageUpload } from './ImageUpload';
-export { VideoSettings } from './VideoSettings';
-export { VideoPreview } from './VideoPreview';
-export type { VideoConfig } from './VideoGenerator';
